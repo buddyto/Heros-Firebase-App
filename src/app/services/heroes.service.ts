69f06615d08f691df6aa0dc351032ebd0f5ff731
Gaspar Aufranc <gaspar.aufranc@gmail.com>
2021-06-08 20:02:41 -0300

    Initial commit

---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeroeModel } from '../pages/models/heroe.model';
+import {map} from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private url = 'https://heros-app-89fca-default-rtdb.firebaseio.com/'
+
+  constructor(private http: HttpClient) { }
+
+  crearHeroe(heroe: HeroeModel){
+
+    return this.http.post(`${this.url}/heroes.json`, heroe).pipe(
+      map((resp:any) => {
+        heroe.id = resp.name;
+        return heroe;
+      })
+    )
+  }
+  getHeroe(id: string){
+    return this.http.get(`${this.url}/heroes/${id}.json`)
+  }
+
+
+  borrarHeroe(id:string){
+    return this.http.delete(`${this.url}/heroes/${id}.json`)
+  }
+
+
+
+
+  actualizarHeroe ( heroe : HeroeModel){
+
+    const heroeTemp = {
+      ...heroe
+    };
+
+    delete heroeTemp.id
+
+    return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp)
+  }
+  getHeroes(){
+    return this.http.get(`${this.url}/heroes.json`)
+            .pipe(
+              map(resp =>  this.crearArreglo(resp))
+            )
+  }
+
+  private crearArreglo(heroesObj: object){
+
+    const heroes: HeroeModel[] = []
+
+    if ( heroesObj === null){
+      return [];
+    }
+    Object.keys(heroesObj).forEach(key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push(heroe)
+    })
+
+    return heroes;
+  }
+}
